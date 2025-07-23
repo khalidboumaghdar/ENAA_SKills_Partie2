855212040ba66a853fa7334e8cb29050966af002
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-07-23 09:53:52 +0000

    sonar qub

---
+++ val-service/src/main/java/com/aplication/valservice/ValServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.valservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValServiceApplication.class, args);
+    }
+
+}
---
+++ val-service/src/test/java/com/aplication/valservice/ValServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.valservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
